cf50c6d9b537a6d0fc8137941f058bf953fc761a
HFouch <40229797+HFouch@users.noreply.github.com>
2020-07-24 14:32:49 +0200

    Update getting_the_data.py

--- getting_the_data.py
+++ getting_the_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import statistics as stats
 from matplotlib import pyplot as plt
-
+from math import log
 
 class DataExtraction:
 
@@ -17,14 +17,7 @@ class DataExtraction:
         #df_number_of_solutions = pd.DataFrame()
         df_operation_types = pd.DataFrame()
 
-        df_inversions = pd.DataFrame()
-        df_transpositions = pd.DataFrame()
-        df_balanced_translocations = pd.DataFrame()
-        df_unbalanced_translocations = pd.DataFrame()
-        df_fissions = pd.DataFrame()
-        df_fusions = pd.DataFrame()
-        df_transpositions2 = pd.DataFrame()
-
+     
 
         df_number_of_operation_types = pd.DataFrame()
 
@@ -35,6 +28,12 @@ class DataExtraction:
         #list containing a tuple for each csv file consisting of (average time genolve took, standard deviation)
 
         percentage_true_solution_present = []
+
+        # BOXPLOTS
+        boxplot_time = []
+        box_plot_num_sol =[]
+
+
         #list containing a percentage value for how often the 'true solution' was present in the set of solutions
         for i in range(1,self.number_of_csv_files+1):
 
@@ -43,9 +42,17 @@ class DataExtraction:
 
             df = pd.read_csv(filename, converters={'Operation_sequences':eval, 'Solution_sequence':eval})
 
+            #BOXPLOTS
+            boxplot_time.append(df['Genolve_time'])
+
+
             #time Genolve took
-            genolve_time_mean = (df['Genolve_time'].mean())
-            genolve_time_std = (df['Genolve_time'].std())
+            genolve_time_mean = (stats.mean(df['Genolve_time']))
+            #new_means = [log(x,10) for x in df['Genolve_time']] ##############
+            #genolve_time_std = stats.stdev(new_means)
+            genolve_time_std = stats.stdev(df['Genolve_time'])
+            genolve_time_min = (df['Genolve_time'].min())
+
             genolve_time.append((genolve_time_mean,genolve_time_std))
 
             # Percentage time true solution was in solution set:
@@ -61,19 +68,10 @@ class DataExtraction:
             # Number of solutions
             number_of_solutions = []
 
-            #Operation types
-            #operation_types_op_sequence =[]
-            #operation_tytpe = [num_inv, num_trp, num_b_trl, num_u_trl, num_fis, num_fus, num_trp2]
 
-            inversions =[]
-            transpositions = []
-            balanced_translocations = []
-            unbalanced_transloctions = []
-            fissions = []
-            fusions =[]
-            transpositions2 = []
             for run in operation_sequences:
-
+                # Number of solutions
+                number_of_solutions.append(len(run))
 
                 # Number of operations per solution
                 lengths = [len(solution) for solution in run]
@@ -85,94 +83,9 @@ class DataExtraction:
                     std = stats.stdev(lengths)
                     solution_lenghts.append((ave, std))
 
-                # Number of solutions
-                number_of_solutions.append(len(run))
 
-                # Operation types
-                operation_types_solution_set =[]
-                for solution in run:
-                    sol_length = len(solution)
-                    sol_operation_types = [(solution.count(0)/sol_length)*100,(solution.count(1)/sol_length)*100 ,(solution.count(2)/sol_length)*100,(solution.count(3)/sol_length)*100, (solution.count(4)/sol_length)*100,(solution.count(5)/sol_length)*100,((solution.count(6.1)+solution.count(6.2))/sol_length)*100]
-                    operation_types_solution_set.append((sol_operation_types))
-
-                #op_type_stats_solution_set = []
-                inv_percentages=[]
-                trp_percentages=[]
-                b_trl_percentages=[]
-                u_trl_percentages=[]
-                fis_percentages=[]
-                fus_percentages=[]
-                trp2_percentages=[]
-                for op_type_percentages in operation_types_solution_set:
-                    inv_percentages.append(op_type_percentages[0])
-                    trp_percentages.append(op_type_percentages[1])
-                    b_trl_percentages.append(op_type_percentages[2])
-                    u_trl_percentages.append(op_type_percentages[3])
-                    fis_percentages.append(op_type_percentages[4])
-                    fus_percentages.append(op_type_percentages[5])
-                    trp2_percentages.append(op_type_percentages[6])
-
-                inv_percentages_mean = stats.mean(inv_percentages)
-                if len(inv_percentages) < 3:
-                    inv_percentages_std = 0
-                else:
-                    inv_percentages_std = stats.stdev(inv_percentages)
-                #op_type_stats_solution_set.append((inv_percentages_mean, inv_percentages_std))
-                inversions.append((inv_percentages_mean, inv_percentages_std))
-
-
-                trp_percentages_mean = stats.mean(trp_percentages)
-                if len(trp_percentages) < 3:
-                    trp_percentages_std = 0
-                else:
-                    trp_percentages_std = stats.stdev(trp_percentages)
-                #op_type_stats_solution_set.append((trp_percentages_mean, trp_percentages_std))
-                transpositions.append((inv_percentages_mean,inv_percentages_std))
-
-                b_trl_percentages_mean = stats.mean(b_trl_percentages)
-                if len(b_trl_percentages) < 3:
-                    b_trl_percentages_std = 0
-                else:
-                    b_trl_percentages_std = stats.stdev(b_trl_percentages)
-                #op_type_stats_solution_set.append((b_trl_percentages_mean, b_trl_percentages_std))
-                balanced_translocations.append((b_trl_percentages_mean,b_trl_percentages_std))
-
-                u_trl_percentages_mean = stats.mean(u_trl_percentages)
-                if len(u_trl_percentages) < 3:
-                    u_trl_percentages_std = 0
-                else:
-                    u_trl_percentages_std = stats.stdev(u_trl_percentages)
-                #op_type_stats_solution_set.append((u_trl_percentages_mean, u_trl_percentages_std))
-                unbalanced_transloctions.append((u_trl_percentages_mean,u_trl_percentages_std))
-
-                fis_percentages_mean = stats.mean(fis_percentages)
-                if len(fis_percentages) < 3:
-                    fis_percentages_std=0
-                else:
-                    fis_percentages_std = stats.stdev(fis_percentages)
-                #op_type_stats_solution_set.append((fis_percentages_mean, fis_percentages_std))
-                fissions.append((fis_percentages_mean, fis_percentages_std))
-
-                fus_percentages_mean = stats.mean(fus_percentages)
-                if len(fus_percentages) < 3:
-                    fus_percentages_std = 0
-                else:
-                    fus_percentages_std = stats.stdev(fus_percentages)
-                #op_type_stats_solution_set.append((fus_percentages_mean, fus_percentages_std))
-                fusions.append((fus_percentages_mean,fus_percentages_std))
-
-
-                trp2_percentages_mean = stats.mean(trp2_percentages)
-                if len(trp_percentages) < 3:
-                    trp2_percentages_std = 0
-                else:
-                    trp2_percentages_std = stats.stdev(trp2_percentages)
-                #op_type_stats_solution_set.append((trp2_percentages_mean, trp2_percentages_std))
-                transpositions2.append((trp2_percentages_mean, trp2_percentages_std))
-
-                #operation_types_op_sequence.append(op_type_stats_solution_set)
-
-
+            #Boxplot data
+            box_plot_num_sol.append(number_of_solutions)
 
 
             num_sol_ave = stats.mean(number_of_solutions)
@@ -187,6 +100,7 @@ class DataExtraction:
             #be calculated and the tuple is (average, False).
             #With the one-to-one weighting scheme there will only be variance between solution lengths for a single run if some of the
             #solutions used block-interchange operations and others did not.
+
 
 
            # df_number_of_solutions[column_name] = number_of_solutions
@@ -207,14 +121,9 @@ class DataExtraction:
             # [inv, trp, b_trl, u_trl, fis, fus, trp2]
             # The total number of rows will thus be equivalent ot the number of runs
 
-            df_inversions[column_name] = inversions
-            df_transpositions[column_name] = transpositions
-            df_balanced_translocations[column_name] = balanced_translocations
-            df_unbalanced_translocations[column_name] = unbalanced_transloctions
-            df_fissions[column_name] = fissions
-            df_fusions[column_name] = fusions
-            df_transpositions2[column_name] = transpositions2
-        return df_solution_lengths_1, num_of_solutions, df_operation_types, genolve_time , percentage_true_solution_present, df_inversions, df_transpositions, df_balanced_translocations, df_unbalanced_translocations, df_fissions, df_fusions, df_transpositions2
+
+
+        return df_solution_lengths_1, num_of_solutions, df_operation_types, genolve_time , percentage_true_solution_present, boxplot_time, box_plot_num_sol #, df_inversions, df_transpositions, df_balanced_translocations, df_unbalanced_translocations, df_fissions, df_fusions, df_transpositions2
 
 
 
